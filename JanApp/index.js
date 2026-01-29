abdc252be7666b278eab052a7b1b527a9f4b20e0
pichu1298 <kristinachuchu@gmail.com>
2026-01-29 16:06:51 -0500

    stuff

--- JanApp/index.js
+++ JanApp/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const port = process.env.PORT || 3000; //using a different port
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 //app.listen how to start server
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
