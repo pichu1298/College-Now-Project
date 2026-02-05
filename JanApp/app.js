2df6c47b4accc1e307fa79144d0d356b0116a3a4
pichu1298 <kristinachuchu@gmail.com>
2026-02-05 16:21:45 -0500

    ?

--- JanApp/app.js
+++ JanApp/app.js
@@ -1,11 +1,36 @@
-const express = require("express");
-const port = process.env.PORT || 3000; //using a different port
+import * as express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 const app = express();
-const routes = require("./Routes/index");
-app.use(express.json()); // -> converts things to json for us
-
-app.use("/", routes); // any URL route in the application is going to use the stuff in .Routes/index file.
-
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
 });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000);
+
+// import { Server } from "socket.io";
+// const io = new Server({});
+
+// io.on("connection", (socket) => {
+//   //...
+// });
+
+// io.listen(3000);
+
+// const express = require("express");
+// const port = process.env.PORT || 3000; //using a different port
+// const app = express();
+// const routes = require("./Routes/index");
+// app.use(express.json()); // -> converts things to json for us
+
+// app.use("/", routes); // any URL route in the application is going to use the stuff in .Routes/index file.
+
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}`);
+// });
--- JanApp/types.ts
+++ JanApp/types.ts
@@ -4,28 +4,51 @@ export type User = {
   password: string;
   profile_picture: string;
 
+  level: Level;
+  stats: UserStats;
   bag: Bag;
   friends: Friend[];
+};
+
+export type UserStats = {
+  id: number;
+  hitpoints: number;
+  physical_attack: number;
+  magical_attack: number;
+  defense: number;
+  agility: number;
+  luck: number;
+};
+
+export type Level = {
+  id: number;
+  level: number;
+  experience: number;
 };
 
 export type Friend = {
   id: number;
   friend_id: number;
-  hitpoints: number;
-  level: number;
+  username: string;
+  nickname: string;
+
+  level: Level;
+  stats: UserStats;
   bag: Bag;
 };
 
 export type Bag = {
   id: number;
   user_id: number;
-  items: BagItem[];
   capacity: number;
+  items: BagItem[];
 };
 
 export type BagItem = {
   id: number;
   name: string;
-  type: "legendary" | "rare" | "common";
+  type: ItemRarity;
   quantity: number;
 };
+
+export type ItemRarity = "legendary" | "epic" | "rare" | "uncommon" | "common";
