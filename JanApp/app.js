b50fb0b1972b3cdb75d1c3611fd706060b0ee709
pichu1298 <kristinachuchu@gmail.com>
2026-02-05 16:57:54 -0500

    hi

--- JanApp/app.js
+++ JanApp/app.js
@@ -4,7 +4,6 @@ const app = express();
 const routes = require("./Routes/index");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use("/", routes);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
