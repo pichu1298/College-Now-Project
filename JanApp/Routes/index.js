295668f059af66ee38aaeb920772a628d4b26983
pichu1298 <kristinachuchu@gmail.com>
2026-02-05 16:38:51 -0500

    ..

--- JanApp/Routes/index.js
+++ JanApp/Routes/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const router = new express.Router();
+const port = process.env.PORT || 3000;
+const app = express();
 
-router.get("/", (req, res) => {
-  res.json("Hello World");
-});
-// : = variable we can do stuff with
-
-router.get("/articles/:title", (req, res) => {
-  try {
-    //attempts to run code, if no work we send to catch
-    res.json(req.params.title);
-  } catch (error) {}
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-module.exports = router;
-
-// json = javascript object notation, universal for every language
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
