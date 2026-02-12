5fbc466fc8be2ec24e325b9c457a6845a0ca33d8
pichu1298 <kristinachuchu@gmail.com>
2026-02-12 17:00:39 -0500

    he

--- JanApp/Routes/index.js
+++ JanApp/Routes/index.js
@@ -1,21 +1,15 @@
 const express = require("express");
-const router = new express.Router();
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+// Example route
 router.get("/", (req, res) => {
-  res.json("Hello World");
-});
-//access URl params
-
-router.get("/articles/:title", (req, res) => {
-  try {
-    //attempt to run code, if no work we send to catch
-
-    res.json(req.params.title);
-  } catch (error) {}
+  res.send("Hello from routes!");
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.body);
-  const { username, password } = req.body;
-  res.json(`${username} is logged in`);
-});
+// Login route
+router.post("/login", userController.login);
+
+// Export the router
 module.exports = router;
